7e04a4856714b44d048c792fbbd3d7fcd7bd027c
edwinb-ai <developeredwin@gmail.com>
2020-11-22 20:05:17 -0600

    :pencil:
    Added some documentation for the generic training and prediction methods.

--- src/training.jl
+++ src/training.jl
@@ -1,3 +1,18 @@
+"""
+    svmtrain(svm::LSSVC, x::AbstractMatrix, y::AbstractVector) -> Tuple
+
+Solves a Least Squares Support Vector Classification problem using the Conjugate Gradient method. In particular, it uses the Lanczos version due to the fact that the matrices are symmetric.
+
+# Arguments
+- `svm::LSSVC`: The Support Vector Machine that contains the hyperparameters, as well as the kernel to be used.
+- `x::AbstractMatrix`: The data matrix with the features. It is expected that this array is already standardized, i.e. the mean for each feature is zero and its standard deviation is one.
+- `y::AbstractVector`: A vector that contains the classes. It is expected that there are only two classes, -1 and 1.
+
+# Returns
+- `Tuple`: A tuple containing `x`, `y` and the following two elements:
+    - `b`: Contains the bias for the decision function.
+    - `α`: Contains the weights for the decision function.
+"""
 function svmtrain(svm::LSSVC, x::AbstractMatrix, y::AbstractVector)
     n = size(y, 1)
     # Initialize the necessary matrices
@@ -20,6 +35,19 @@ function svmtrain(svm::LSSVC, x::AbstractMatrix, y::AbstractVector)
     return (x, y, α, b)
 end
 
+"""
+    svmpredict(svm::LSSVC, fits, xnew::AbstractMatrix) -> AbstractArray
+
+Uses the information obtained from `svmtrain` such as the bias and weights to construct a decision function and predict new class values.
+
+# Arguments
+- `svm::LSSVC`: The Support Vector Machine that contains the hyperparameters, as well as the kernel to be used.
+- `fits`: It can be any container data structure but it must have four elements: `x`, the data matrix; `y`, the labels vector; `α`, the weights; and `b`, the bias.
+- `xnew::AbstractMatrix`: The data matrix that contains the new instances to be predicted.
+
+# Returns
+- `Array`: The labels corresponding to the prediction to each of the instances in `xnew`.
+"""
 function svmpredict(svm::LSSVC, fits, xnew::AbstractMatrix)
     x, y, α, b = fits
     # Compute the asymmetric kernel matrix in one go
@@ -33,6 +61,28 @@ function svmpredict(svm::LSSVC, fits, xnew::AbstractMatrix)
     return sign.(result)
 end
 
+@doc raw"""
+    build_omega(x::AbstractMatrix, y::AbstractVector, sigma::Float64; kernel::String="rbf") -> AbstractMatrix
+
+It builds a matrix, known as the "omega matrix", that contains the following information
+$$
+\Omega_{kl} = y_k y_l K(x_k,x_l)
+$$
+with $k,l=1,\dots,N$, and $N$ being the length of `x`. In other words, the number of training instances.
+
+This matrix contains information about the mapping to a new space using the kernel. It is exclusively used in the training step of the learning procedure.
+
+# Arguments
+- `x::AbstractMatrix`: The data matrix with the training instances.
+- `y::AbstractVector`: The labels for each of the instances in `x`.
+- `sigma::Float64`: The hyperparameter for the RBF kernel.
+
+# Keywords
+- `kernel::String="rbf"`: The kernel to be used. For now, only the RBF kernel is implemented.
+
+# Returns
+- `Ω`: The omega matrix computed as shown above.
+"""
 function build_omega(
     x::AbstractMatrix,
     y::AbstractVector,
