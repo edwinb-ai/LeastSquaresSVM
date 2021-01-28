9bb0ca0cad89ff6166ce04d679afbe73d26e8a68
edwinb-ai <developeredwin@gmail.com>
2021-01-28 11:24:05 -0600

    :recycle:
    Disable multithreading from Tullio and change to Conjugate Gradient.
    Apparently, some solvers did not converge.

--- src/tools.jl
+++ src/tools.jl
@@ -1,17 +1,17 @@
 """
     An external product using Einstein summation.
 """
-extern_prod(x, y) = @tullio z[i, j] := x[i] * y[j]
+extern_prod(x, y) = @tullio threads = false z[i, j] := x[i] * y[j]
 
 """
     Pairwise multiplication when `x` and `a` both have two dimensions.
 """
-pairwise_mul!(x, a) = @tullio x[i, j] *= a[i, j]
+pairwise_mul!(x, a) = @tullio threads = false x[i, j] *= a[i, j]
 
 """
     Pairwise difference when `x` and `y` are vectors.
 """
-pairwise_diff(x, y) = @tullio z[i] := x[i] - y[i]
+pairwise_diff(x, y) = @tullio threads = false z[i] := x[i] - y[i]
 
 """
     Computes a pairwise multiplication between `y`, `z` and the second dimension from
@@ -23,6 +23,6 @@ It is equivalent to
 sum(@. x * y * z; dims=1)
 ```
 """
-prod_reduction(x, y, z) = @tullio w[1, j] := x[i, j] * y[i] * z[i]
+prod_reduction(x, y, z) = @tullio threads = false w[1, j] := x[i, j] * y[i] * z[i]
 
-prod_reduction(x, y) = @tullio w[1, j] := x[i, j] * y[i]
+prod_reduction(x, y) = @tullio threads = false w[1, j] := x[i, j] * y[i]
