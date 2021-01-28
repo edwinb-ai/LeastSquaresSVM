765e51d559bc819a7e0ec2cdd07dd3f10d7d62df
edwinb-ai <developeredwin@gmail.com>
2021-01-27 18:54:55 -0600

    :memo:
    Added some docstrings.

--- src/tools.jl
+++ src/tools.jl
@@ -13,4 +13,14 @@ pairwise_mul!(x, a) = @tullio x[i, j] *= a[i, j]
 """
 pairwise_diff(x, y) = @tullio z[i] := x[i] - y[i]
 
+"""
+    Computes a pairwise multiplication between `y`, `z` and the second dimension from
+`x`. Then, it uses a sum reduction of the first dimension from `x`.
+
+It is equivalent to
+
+```julia
+sum(@. x * y * z; dims=1)
+```
+"""
 prod_reduction(x, y, z) = @tullio w[1, j] := x[i, j] * y[i] * z[i]
