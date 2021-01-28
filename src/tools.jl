ad198709d0e9a9882109547578b623b36f7bf1b6
edwinb-ai <developeredwin@gmail.com>
2021-01-27 18:18:49 -0600

    :memo:
    Added docstrings to tullio tools.

--- src/tools.jl
+++ src/tools.jl
@@ -1,5 +1,14 @@
+"""
+    An external product using Einstein summation.
+"""
 extern_prod(x, y) = @tullio z[i, j] := x[i] * y[j]
 
+"""
+    Pairwise multiplication when `x` and `a` both have two dimensions.
+"""
 pairwise_mul!(x, a) = @tullio x[i, j] *= a[i, j]
 
+"""
+    Pairwise difference when `x` and `y` are vectors.
+"""
 pairwise_diff(x, y) = @tullio z[i] := x[i] - y[i]
