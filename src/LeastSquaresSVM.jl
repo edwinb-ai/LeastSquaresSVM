b652182922d2fc9c68c2eaa0296461aad901d4df
Edwin Bedolla <developeredwin@gmail.com>
2021-02-09 15:11:18 -0600

    :art:
    Added all the files and types to the main file. Still not working,
    but a WIP.

--- src/LeastSquaresSVM.jl
+++ src/LeastSquaresSVM.jl
@@ -11,12 +11,13 @@ const MMI = MLJModelInterface
 const BL = LinearAlgebra.BLAS
 
 export SVM, LSSVC, LSSVR, KernelRBF, svmtrain, svmtrain_mc, svmpredict, LSSVClassifier,
-LSSVRegressor
+LSSVRegressor, FixedSizeSVR
 
 include("types.jl")
 include("utils.jl")
 include("tools.jl")
 include("training.jl")
+include("fixed_training.jl")
 include("mlj_interface.jl")
 # Some precompilation for the Tullio tools
 include("precompile.jl")
--- src/fixed_training.jl
+++ src/fixed_training.jl
@@ -77,7 +77,8 @@ function svmtrain(svm::FixedSizeSVR, x::AbstractMatrix, y::AbstractVector)
     # Compute the weights for the decision function
     alphas = m .* fact.vectors
     @. alphas /= sqrt(fact.values)
-    result = sum(alphas .* wi_s; dims=1)
+    # result = sum(alphas .* wi_s; dims=1)
+    result = prod_reduction(alphas, wi_s)
 
     return (result, bias, idxs)
 end
